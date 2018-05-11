af81ad3d2e41d04507a8844abd165173b087c2ab
Ergellon <georgiychugreev@gmail.com>
2018-05-11 18:35:26 +0300

    testing pun connection

---
+++ WizCloneProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Photon.PunBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    void LoadArena()
+    {
+        if (!PhotonNetwork.isMasterClient)
+        {
+            Debug.LogError("not master client");
+        }
+        PhotonNetwork.LoadLevel("Battle");
+    }
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log(other.NickName);
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("massster");
+            LoadArena();
+        }
+    }
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+    {
+        Debug.Log(other.NickName + "left");
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("massster left");
+        }
+    }
+
+}
