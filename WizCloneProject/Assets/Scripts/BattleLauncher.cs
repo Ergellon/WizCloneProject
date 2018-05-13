23d57cfb90d11bdafa39158b02325358e7e503f0
Ergellon <georgiychugreev@gmail.com>
2018-05-14 01:39:24 +0300

    not copypaste at last

---
+++ WizCloneProject/Assets/Scripts/BattleLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleLauncher : Photon.MonoBehaviour {
+
+    public string playername = "defaultplayer";
+    public string enemyname = "defaultenemy";
+
+    public InputField inputname;
+
+	void Start () {
+
+        DontDestroyOnLoad(this);
+	}
+	
+	void Update () {
+		
+	}
+
+    public void ChangePlayerName()
+    {
+        if (photonView.isMine == true)
+        {
+            playername = inputname.text;
+        }
+        else
+        {
+            enemyname = inputname.text;
+        }
+    }
+}
---
+++ WizCloneProject/Assets/Scripts/ConnectionLauncher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ConnectionLauncher : Photon.PunBehaviour {
+
+    string gameversion = "0.01";
+
+    public GameObject connecting;
+
+    void Awake()
+    {
+        PhotonNetwork.automaticallySyncScene = true; 
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Connect ()
+    {
+        connecting.SetActive(true);
+        
+        if (PhotonNetwork.connectedAndReady)
+        {
+            PhotonNetwork.JoinRandomRoom(); //maybe try JoinOrCreate later
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings(gameversion);
+        }
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+
+
+}
